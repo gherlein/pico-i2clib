ab81f5ced8a7401ac14ff31b94edd4e62fe79117
Greg Herlein <gherlein@herlein.com>
2024-09-04 16:57:16 -0700

    updated

--- include/pico-i2clib.h
+++ include/pico-i2clib.h
@@ -1,2 +1,14 @@
+
+
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "pico/types.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
 i2c_inst_t *pico_init_i2c(i2c_inst_t *i2c, uint baudrate, uint8_t sdaPin, uint8_t sclPin);
 void pico_scan_i2c(i2c_inst_t *i2c);
--- src/i2clib.c
+++ src/i2clib.c
@@ -6,25 +6,7 @@
 
 */
 
-#include "hardware/gpio.h"
-#include "hardware/i2c.h"
-#include "pico/binary_info.h"
-#include "pico/stdlib.h"
-#include "pico/time.h"
-#include "pico/types.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 #include "pico-i2clib.h"
-
-typedef struct i2c_vars_t
-{
-    i2c_inst_t *i2c;
-    int baudrate;
-    uint8_t sdaPin;
-    uint8_t sdcPin;
-    bool ready;
-} i2c_var;
 
 static bool reserved_addr(uint8_t addr)
 {
@@ -33,11 +15,6 @@ static bool reserved_addr(uint8_t addr)
 
 i2c_inst_t *pico_init_i2c(i2c_inst_t *i2c, uint baudrate, uint8_t sdaPin, uint8_t sclPin)
 {
-    i2c_var.i2c = i2c;
-    i2c_var.baudrate = buadrate;
-    i2c_var.sdaPin = sdaPin;
-    i2c_var.sdcPin = sdcPin;
-
     i2c_init(i2c, 100 * 1000);
     gpio_set_function(sdaPin, GPIO_FUNC_I2C);
     gpio_set_function(sclPin, GPIO_FUNC_I2C);
@@ -45,8 +22,6 @@ i2c_inst_t *pico_init_i2c(i2c_inst_t *i2c, uint baudrate, uint8_t sdaPin, uint8_
     gpio_pull_up(sclPin);
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-    i2c_var.ready = true;
 
     return i2c;
 }
